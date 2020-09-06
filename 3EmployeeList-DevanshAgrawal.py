a7c1046c2e3a175f8c45e44a437da19c805cb271
Devansh Agrawal <devagr3924@gmail.com>
2020-09-06 09:49:40 -0500

    Added input verification, setup for user redirect

--- 3EmployeeList-DevanshAgrawal.py
+++ 3EmployeeList-DevanshAgrawal.py
@@ -3,7 +3,7 @@ from tempfile import NamedTemporaryFile
 
 class EmployeeList:
     def __init__(self):
-        self.user_input=""
+        self.user_input = ""
         self.display_main_menu()
 
     def display_main_menu(self):
@@ -19,15 +19,50 @@ class EmployeeList:
                 7) Quit
             """
         )
-        self.user_input=int(input("Enter number for choice: "))
+        self.user_input=self.take_input("int")
+
+    def take_input(self, data_type):
+        if data_type == "int":
+            while True:
+                try:
+                    self.user_input = int(input('Insert choice: '))
+                    break
+                except:
+                    print("try again")
+        
+        elif data_type == "string":
+            while True:
+                try:
+                    self.user_input = str(input("Insert choice: "))
+                    break
+                except:
+                    print("try again")
 
     def redirect_user(self):
         if type(self.user_input) == int:
             if self.user_input == 1:
-                ###add choices
+                #add
+                pass
+            elif self.user_input == 2:
+                #delete
+                pass
+            elif self.user_input == 3:
+                #sort
+                pass
+            elif self.user_input == 4:
+                #search
+                pass
+            elif self.user_input == 5:
+                #save
+                pass
+            elif self.user_input == 6:
+                #display
+                pass
+            elif self.user_input == 7:
+                #quit
                 pass
         else:
-            print("Please enter a number.")
+            print("Please enter a number from 1-7.")
             self.display_main_menu()
 
-the_employee_list=EmployeeList()
+the_employee_list = EmployeeList()
